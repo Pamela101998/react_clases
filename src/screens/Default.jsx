aef940420b3bec05dd1c8f662106837649e7162e
pamela <pamelanayda1998@example.com>
2024-08-20 00:28:44 -0400

    commit openlink

--- src/screens/Default.jsx
+++ src/screens/Default.jsx
@@ -1,7 +1,7 @@
-import Logo from "../components/Logo";
-import Description from "../components/Description";
-import Content from "../components/Content";
-import GitHub from "../components/GitHub";
+import Logo from './_components/Logo';
+import Description from "./_components/Description";
+import OpenLink from "../components/OpenLink";
+
 
 const Default = () => {
     return (
@@ -9,8 +9,15 @@ const Default = () => {
             <header className="App-header">
                 <Logo />
                 <Description />
-                <Content />
-                <GitHub />
+                <OpenLink 
+                    title="Learn React" 
+                    url="https://reactjs.org" 
+                />
+
+                <OpenLink 
+                    title="Mi proyecto React (Git)" 
+                    url="https://github.com/Pamela101998/react_clases" 
+                />
             </header>
         </>
     );
